47efdf5ebf1972493dc357a76ccc1cfca6ce6974
jeffreyxiong <jeffxiong017@gmail.com>
2016-01-16 12:08:32 -0600

    routing js

---
+++ delivery/lib/router.js
@@ -0,0 +1,43 @@
+Router.configure({
+	// general routing configuration
+
+	// general template
+	// layoutTemplate: 'main'
+});
+
+
+// login routes
+Router.route('/venmo_login', {
+	template: 'venmo_login'
+});
+
+Router.route('/google_login', {
+	template: 'google_login'
+});
+
+Router.route('/', {
+	name: 'home',
+	template: 'home'
+});
+
+Router.route('/order', {
+	template: 'order'
+});
+
+Router.route('/accept', {
+	template: 'accept'
+});
+
+
+// later routes
+Router.route('/history', {
+	template: 'history'
+});
+
+Router.route('/profile', {
+	template: 'profile'
+});
+
+Router.route('/active', {
+	template: 'active'
+});
