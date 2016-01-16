c2bfdcda197d34aae61d53a2f406f92eabb1cf1a
Lance <lclamotte@rice.edu>
2016-01-16 10:35:20 -0600

    server-side venmo

--- delivery/server/orders.js
+++ delivery/server/orders.js
@@ -14,4 +14,32 @@ if (Meteor.isClient) {
 			})
 		}
 	});
+	Meteor.loginWithVenmo(function (err) {
+  		if (err) {
+    		throw new Meteor.Error("login-failed", 
+      		"Authentication with Venmo failed");
+  		}
+});
+	Meteor.logout(function(err){
+  		if (err) {
+    		throw new Meteor.Error("logout-failed",
+      		"Log out failed");
+  		}
+	});
+};
+
+
+if (Meteor.isServer) {
+	Meteor.startup(function () {
+
+		ServiceConfiguration.configurations.upsert({
+		  service: "venmo"
+		  }, {
+		    $set {
+		      clientId: "3428",
+		      scope: "make_payments + access_email + access_balance",
+		      secret: "efFYfmDRbygAsmNHmffXMhaCAFt8GG5N"
+		    }
+		});	
+	})
 }
