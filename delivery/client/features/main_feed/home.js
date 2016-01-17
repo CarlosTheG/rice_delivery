a7da0b83a57d48b64b4c7c4298af17e42216ff84
zgramstad <zeg2@rice.edu>
2016-01-17 02:03:50 -0600

    Fixed Pending delivery

--- delivery/client/features/main_feed/home.js
+++ delivery/client/features/main_feed/home.js
@@ -25,7 +25,7 @@ Template.individual_order.events({
         type: "warning",   showCancelButton: true,   confirmButtonColor: "#008fb3",   
         confirmButtonText: "Yes, I accept!",   cancelButtonText: "No, take me back!",   
         closeOnConfirm: false,   closeOnCancel: true }, 
-        function(isConfirm, id){   
+        function(isConfirm){   
           if (isConfirm) {     
             swal("Sweet!", "Check your feed for delivery details.", "success");
             // Need to update deliverer
