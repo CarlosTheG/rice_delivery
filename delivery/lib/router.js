621b1239885f76411cc32efb674803a3bd22e507
jeffreyxiong <jeffxiong017@gmail.com>
2016-01-16 22:55:46 -0600

    venmo authentication

--- delivery/lib/router.js
+++ delivery/lib/router.js
@@ -8,21 +8,56 @@ Router.configure({
 // venmo routes
 Router.route('/venmo/:clientId/:scope', {where: 'server'}).get(function() {
 	this.response.writeHead(302, {
-    	'Location': "https://api.venmo.com/v1/oauth/authorize?client_id=" + this.params.clientId + "&scope=" + this.params.scope
+    	'Location': "https://api.venmo.com/v1/oauth/authorize?client_id=" + this.params.clientId + "&scope=" + this.params.scope 
+    	// TODO: ADD THIS FOR SERVER-SIDE:
+    	//+ "&response_type=code"
   	});
   	console.log('redirecting...');
   	this.response.end();
 });
 
+// CLIENT-SIDE FLOW AUTH-TOKEN
 Router.route('/_oauth/:accessToken', {
-	data: function(){
-		var token = this.params.query
-		console.log(token);
-    }
-});
+	data: function() {
+		var token = this.params.query;
+		console.log(token[access_token]);
+		Meteor.users.update(Meteor.userId(), {$set: {"profile.venmo": true}});
+		Meteor.users.update(Meteor.userId(), {$set: {"profile.authToken": token[access_token]}});
+		this.redirect('/');
+	}
+})
 
 
-
+// TODO: SERVER-SIDE FLOW AUTH-TOKEN 
+	// Router.route('/_oauth/:authToken', {
+	// 	data: function(){
+	// 		var token = this.params.query;
+	// 		var code = token.code;
+	// 		var response = HTTP.call('POST', "https://api.venmo.com/v1/oauth/access_token", {
+	// 			data: {
+	// 				"client_id": "3428",
+	// 				"client_secret": "efFYfmDRbygAsmNHmffXMhaCAFt8GG5N",
+	// 				"code": code
+	// 			},
+	// 			options: {
+	// 				headers: {
+	// 			      	'Access-Control-Allow-Origin': '*',
+	// 			       	'Access-Control-Allow-Methods': 'POST, PUT, DELETE, GET, OPTIONS',
+	// 			       	'Access-Control-Request-Method': '*',
+	// 			       	'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+	// 			    }
+	// 			}
+	// 		}, function(error, response) {
+	// 			if (error) {
+	// 				console.log('FUCK');
+	// 				console.log(error);
+	// 			} else {
+	// 				response.setHeader( 'access-control-allow-origin', '*' );
+	// 				console.log(response);
+	// 			}
+	// 		});
+	//     }
+	// }, {where: 'server'});
 
 // login routes
 
