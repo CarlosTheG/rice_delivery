c8128570460184d221694c5df2c48e9eadc7419d
Carlos Gonzalez <Carlos@Carloss-MacBook-Pro.local>
2016-01-16 19:09:26 -0600

    Updates to create_order

--- delivery/client/features/create_order/create_order.js
+++ delivery/client/features/create_order/create_order.js
@@ -1,10 +1,26 @@
 var curr_Order = [];
 var order_sum = Session.set("sum", 1.50);
+var size = Session.set("size", "small");
+
+var business_list = ["Coffee House", "Hoot"]; //todo: misc
+
 var hoot_menu = [
 	{name: "HBCB", price:2.50, checked:false}, 
 	{name: "Other stuff", price:5, checked:false}, 
-	{name:"expensive stuff", price: 6, checked:false}
+	{name: "expensive stuff", price: 6, checked:false}
 ];
+
+var coffee_size = [
+	{name: "small", checked: true}, 
+	{name: "medium", checked: false},
+	{name:"large", checked: false}
+];
+var coffee_menu = [
+	{name: "frap", price: 5, checked: false }
+];
+
+var coffee_selected = {};
+
 
 Template.order_form.helpers({
 
@@ -16,8 +32,21 @@ Template.order_form.helpers({
 		return hoot_menu;
 	},
 
+	coffee_menu: function(){
+		return coffee_menu;
+	},
+
 	sum: function(){
-		return Session.get("sum");
+		var hoot_sum = Session.get("sum");
+		var selected_size = Session.get("size")
+
+		var selected_size = $("input:radio[name='size']:checked").val();
+		var coffee_sum = 0;
+		if (selected_size === "small") {
+			coffee_sum = 1;
+		}
+
+		return hoot_sum + coffee_sum;
 	},
 })
 
@@ -33,29 +62,28 @@ Template.order_form.events({
     	// var text = event.target.text.value;
 
 	  	Orders.insert({
-			price: Session.set("sum"),
-	        createdAt: new Date() // current time
+			price: Session.get("sum"),
+	        createdAt: new Date(), // current time
+	        status: "active"
 		});
+
+		Router.go("/");
     }
 });
 
 Template.menu_items.events({
 	'click .toggle_check': function() {
-		var returned_array = checkedHelper(this.name, hoot_menu);
-
-		var price = returned_array[1];
-		if (returned_array[0] == true) { 
-			Session.set("sum", Session.get("sum") + price);
+		this.checked = !this.checked;
+		if (this.checked == true) { 
+			Session.set("sum", Session.get("sum") + this.price);
 			curr_Order.push(this.name);
 		} else { 
-			Session.set("sum", Session.get("sum") - price);
+			Session.set("sum", Session.get("sum") - this.price);
 			var index = curr_Order.indexOf(this.name);
 			if (index > -1) {
 				curr_Order.splice(index, 1);
 			}
 		}
-
-
 	}
 });
 
@@ -72,13 +100,3 @@ Template.menu_items.events({
 // 		}
 // 	}
 // })
-
-function checkedHelper(element_name, obj_array) {
-	for (var i in obj_array) {
-		if (obj_array[i].name == element_name) {
-			obj_array[i].checked = !obj_array[i].checked;
-			break;
-		}
-	}
-	return [obj_array[i].checked, obj_array[i].price]
-}
--- delivery/client/features/main_feed/home.js
+++ delivery/client/features/main_feed/home.js
@@ -4,3 +4,9 @@ Template.home_feed.helpers({
 		return Orders.find();
 	}
 });
+
+Template.individual_order.events({
+    'click .delete' : function() {
+      Orders.remove(this._id);
+    }
+  });
